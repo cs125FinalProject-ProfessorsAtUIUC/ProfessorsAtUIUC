1b0a59b402cc97ee4ad0a5cdb66dd66ae2fe07a1
ruoxiw3 <ruoxiw3@illinois.edu>
2019-12-05 07:14:16 +0800

    Professor 按钮转跳。目前一切运行正常。

--- app/src/main/java/com/professorsatuiuc/MainActivity.java
+++ app/src/main/java/com/professorsatuiuc/MainActivity.java
@@ -2,7 +2,10 @@ package com.professorsatuiuc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class
 
@@ -12,5 +15,12 @@ MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button toSearchProf = findViewById(R.id.toSearchProf);
+        toSearchProf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SearchByProfessor.class));
+            }
+        });
     }
 }
