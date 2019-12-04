95862e6f4a540f9681bce3dcd6105fbdd1485d0c
Haoen-Li <55057954+Haoen-Li@users.noreply.github.com>
2019-12-04 15:14:39 -0600

    把csv中的信息提取成professor list

--- app/src/main/java/com/professorsatuiuc/Professor.java
+++ app/src/main/java/com/professorsatuiuc/Professor.java
@@ -8,14 +8,12 @@ import java.util.Scanner;
 
 public class Professor {
     private String name;
-    private String subject;
     private String courseNum;
     private String courseTitle;
     private String yearTerm;
     private String grade;
-    public Professor(String name, String subject, String courseNum, String courseTitle, String yearTerm, String grade) {
+    public Professor(String name, String courseNum, String courseTitle, String yearTerm, String grade) {
         this.name = name;
-        this.subject = subject;
         this.courseNum = courseNum;
         this.courseTitle = courseTitle;
         this.yearTerm = yearTerm;
@@ -30,12 +28,12 @@ public class Professor {
             while (inputStream.hasNext()) {
                 String data = inputStream.next();
                 String[] values = data.split(",");
-                String grade = values[6] + ", " + values[7] + ", " + values[8] + ", " + values[9] + ", "
-                        + values[10] + ", " + values[11] + ", " + values[12] + ",  " + values[13] + ", "
-                        + values[14] + ", " + values[15] + ", " + values[16] + ",  " + values[17] + ", "
-                        + values[18] + ", " + values[19];
-                professorList.add(new Professor(values[20], values[3], values[4],
-                        values[5], values[2], grade));
+                String courseNum =  values[3] + " " + values[4];
+                String grade = "A+: " + values[6] + ", A: " + values[7] + ", A-: " + values[8] + ", B+: "
+                        + values[9] + ", B: " + values[10] + ", B-: " + values[11] + ", C+: " + values[12]
+                        + ",  C: " + values[13] + ", C-: " + values[14] + ", D+: " + values[15] + ", D: "
+                        + values[16] + ", D-: " + values[17] + ", F: " + values[18] + ", W: " + values[19];
+                professorList.add(new Professor(values[20], courseNum, values[5], values[2], grade));
             }
             inputStream.close();
         } catch (FileNotFoundException e) {
