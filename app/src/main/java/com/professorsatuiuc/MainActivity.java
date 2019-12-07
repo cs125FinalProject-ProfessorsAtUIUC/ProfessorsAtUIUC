5e0173576bcb6019fc6dae205e8dff90e7f5d397
Haoen-Li <55057954+Haoen-Li@users.noreply.github.com>
2019-12-06 21:58:34 -0600

    try

--- app/src/main/java/com/professorsatuiuc/MainActivity.java
+++ app/src/main/java/com/professorsatuiuc/MainActivity.java
@@ -7,6 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class
 
 MainActivity extends AppCompatActivity {
@@ -17,12 +26,39 @@ MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //跳到search by professor页
         Button toSearchProf = findViewById(R.id.toSearchProf);
-        Professor.searchProfessor();
+        searchProfessor();
         toSearchProf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, SearchByProfessor.class));
             }
         });
+    }
+
+    public void searchProfessor() {
+        InputStream is = getResources().openRawResource(R.raw.letmesee);
+        try {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//        Reader in = new FileReader();
+//        Iterable<CSVRecord> records = null;
+//        try {
+//            records = CSVFormat.EXCEL.parse(in);
+//            for (CSVRecord record : records) {
+//                String prof = record.get("Primary Instructor");
+//                System.out.println(prof);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] rowData = line.split(",");
+                System.out.println(rowData[0]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
