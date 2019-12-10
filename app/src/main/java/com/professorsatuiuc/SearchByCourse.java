e4d4536c2b2de601e5f8c989cd9b586770a16e39
Haoen-Li <55057954+Haoen-Li@users.noreply.github.com>
2019-12-09 22:51:00 -0600

    commit

--- app/src/main/java/com/professorsatuiuc/SearchByCourse.java
+++ app/src/main/java/com/professorsatuiuc/SearchByCourse.java
@@ -1,6 +1,7 @@
 package com.professorsatuiuc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.textservice.SuggestionsInfo;
@@ -8,9 +9,6 @@ import android.view.textservice.TextInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-
-import org.w3c.dom.Text;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -18,8 +16,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 public class SearchByCourse extends AppCompatActivity {
@@ -28,7 +24,7 @@ public class SearchByCourse extends AppCompatActivity {
     Button searchCourse_button;
     EditText searchCourse_text;
     LinearLayout courseList;
-    private Map<String, LinkedList<String[]>>  courses = new HashMap<>();
+    private Map<String, ArrayList<String>>  courses = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,43 +76,35 @@ public class SearchByCourse extends AppCompatActivity {
         LinearLayout courseList = findViewById(R.id.courseList);
         courseList.removeAllViews();
         final InputStream is = getResources().openRawResource(R.raw.gpa);
-
-
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] rowData = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-                String de1 = rowData[3] + " " + rowData[4];
-                String de2 = rowData[3] + rowData[4];
+                final String de1 = rowData[3] + " " + rowData[4];
+                final String de2 = rowData[3] + rowData[4];
                 if (de1.toLowerCase().contains(name.toLowerCase())
                         || de2.toLowerCase().contains(name.toLowerCase())) {
                     try {
-                        courses.get(de1).addFirst(rowData);
+                        courses.get(de1).add(line);
                     } catch (Exception e) {
-                        LinkedList<String[]> toPut = new LinkedList<>();
-                        toPut.add(rowData);
+                        ArrayList<String> toPut = new ArrayList<>();
+                        toPut.add(line);
                         courses.put(de1, toPut);
+                        View courseChunk = getLayoutInflater().inflate(R.layout.chunk_course, courseList, false);
+                        Button aCourse = courseChunk.findViewById(R.id.aCourse);
+                        aCourse.setText(de1);
+                        courseList.addView(courseChunk);
+                        aCourse.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent intent = new Intent(SearchByCourse.this, CourseDisplay.class);
+                                intent.putExtra("name", de1);
+                                intent.putExtra("datas", courses.get(de1));
+                                startActivity(intent);
+                            }
+                        });
                     }
-                    View courseChunk = getLayoutInflater().inflate(R.layout.chunk_course, courseList, false);
-                    Button aCourse = courseChunk.findViewById(R.id.aCourse);
-                    aCourse.setText(de1);
-                    courseList.addView(courseChunk);
-//                    aProf.setOnClickListener(new View.OnClickListener() {
-//                        @Override
-//                        public void onClick(View view) {
-//                            Intent intent = new Intent(SearchByProfessor.this, ProfDisplay.class);
-//                            intent.putExtra("name", name);
-//                            LinkedList<String[]> datas = professors.get(name);
-//                            int i = 0;
-//                            while (datas != null) {
-//                                intent.putExtra(Integer.toString(i), datas.removeFirst());
-//                                i++;
-//                            }
-//                            intent.putExtra("lines", i);
-//                            startActivity(intent);
-//                        }
-//                    });
                 }
             }
         } catch (FileNotFoundException e) {
